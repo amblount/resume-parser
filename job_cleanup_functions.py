a7e35aa9b3267f09f433dedec84cefae4ada0452
Matthew Cassi <matthewcassi@Matthews-MacBook-Pro.local>
2019-10-06 16:42:42 -0400

    adding in changes for the muse

--- job_cleanup_functions.py
+++ job_cleanup_functions.py
@@ -27,26 +27,31 @@ def split_skills(row, column):
     new_val = re.sub('●', '', new_val)
     new_val = re.sub(r'\n', ' ', new_val)
 
-    text_split = new_val.split('<p> | <li>')
+    text_split = new_val.split('<p>|<li>|-')
     list_strings = [clean_html(x) for x in text_split]
     list_strings = [x for x in list_strings]
 
-    list_strings = " ".join(list_strings).strip()
+    new_list_strings = []
+    for i in list_strings:
+        if not i.endswith('.'):
+            new_list_strings.append(i + '.')
 
-    list_strings = re.sub(r'http\S+', ' ', list_strings, flags=re.MULTILINE)
-    list_strings = re.sub("-", " ", list_strings)
-    list_strings = re.sub("d3.js", "dthree.js", list_strings)
-    list_strings = re.sub(re.escape("c++"), "cplusplus", list_strings)
-    list_strings = re.sub(re.escape("c#"), "csharp", list_strings)
-    list_strings = re.sub(re.escape(".net"), "dotnet", list_strings)
-    list_strings = re.sub("java 8", "java eight", list_strings)
-    list_strings = re.sub("python 3", "python three", list_strings)
-    list_strings = re.sub("python 2", "python two", list_strings)
-    list_strings = re.sub(r"\&", "and", list_strings)
-    list_strings = re.sub(r"\\", "", list_strings)
-    list_strings = re.sub(r"\/", "", list_strings)
+    new_list_strings = " ".join(new_list_strings).strip()
 
-    return(list_strings)
+    new_list_strings = re.sub(r'http\S+', ' ', new_list_strings, flags=re.MULTILINE)
+    new_list_strings = re.sub(r'\\', ' ', new_list_strings)
+    new_list_strings = re.sub(r'\/', ' ', new_list_strings)
+    new_list_strings = re.sub("-", " ", new_list_strings)
+    new_list_strings = re.sub("d3.js", "dthree.js", new_list_strings)
+    new_list_strings = re.sub(re.escape("c++"), "cplusplus", new_list_strings)
+    new_list_strings = re.sub(re.escape("c#"), "csharp", new_list_strings)
+    new_list_strings = re.sub(re.escape(".net"), "dotnet", new_list_strings)
+    new_list_strings = re.sub("java 8", "java eight", new_list_strings)
+    new_list_strings = re.sub("python 3", "python three", new_list_strings)
+    new_list_strings = re.sub("python 2", "python two", new_list_strings)
+    new_list_strings = re.sub(r"\&", "and", new_list_strings)
+
+    return(new_list_strings)
 
 def clean_text(row, column):
 
