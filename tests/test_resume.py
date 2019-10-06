b871e30f3de7b7a6c8ad5fd578f3c9a220847db7
Manojit Nandi <mnandi92@gmail.com>
2019-10-06 14:33:34 -0400

    Added initial resume structure

---
+++ tests/test_resume.py
@@ -0,0 +1,26 @@
+import unittest
+from models.resume import Resume
+
+
+class TestResume(unittest.TestCase):
+
+    def setUp(self):
+        self.resume = Resume()
+
+    def test_separate_skills(self):
+        pass 
+    
+    def test_access_attribute(self):
+        pass 
+
+    def test_export_resume(self):
+        pass
+        #self.isinstance()
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
