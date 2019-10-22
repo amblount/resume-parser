e55a6a909caa7298503586d2c08017458f374154
Burton DeWilde <burtdewilde@gmail.com>
2019-10-22 12:26:44 -0500

    Clarify relationship bw CRF and rules skills parsing

--- msvdd_bloc/resumes/parse/skills.py
+++ msvdd_bloc/resumes/parse/skills.py
@@ -6,65 +6,6 @@ import re
 
 LOGGER = logging.getLogger(__name__)
 
-#########################
-## RULES-BASED PARSING ##
-#########################
-
-RE_SKILL_NAME_DELIM = re.compile(r"(?:: +| +[-–] +)")
-RE_SKILL_KEYWORD_DELIM = re.compile(r"[,;] +(?:(?:and|&) +)?")
-RE_SKILL_LEVEL = re.compile(
-    r"\(?(?P<level>advanced|intermediate|beginner|basic|proficient|exposed)\)? ?(in )?",
-    flags=re.UNICODE | re.IGNORECASE,
-)
-RE_SKILL_CLEAN = re.compile(r"[.,;]$")
-
-
-def parse_skills_section(lines):
-    """
-    Parse a sequence of text lines belonging to the "skills" section of a résumé
-    to produce structured data in the form of :class:`schemas.ResumeSkillSchema`.
-
-    Args:
-        lines (List[str])
-
-    Returns:
-        List[Dict[str, str]]
-    """
-    all_skills = []
-    for line in lines:
-        if not line:
-            continue
-        line = line.lstrip("- ")
-        split_line = RE_SKILL_NAME_DELIM.split(line)
-        if len(split_line) == 1:
-            names = RE_SKILL_KEYWORD_DELIM.split(line)
-            if len(names) == 1:
-                names = line.split()
-            skills = []
-            for name in names:
-                match = RE_SKILL_LEVEL.search(name)
-                if match:
-                    skill = {
-                        "name": RE_SKILL_CLEAN.sub("", RE_SKILL_LEVEL.sub("", name).strip()),
-                        "level": match.group("level"),
-                    }
-                else:
-                    skill = {"name": RE_SKILL_CLEAN.sub("", name.strip())}
-                skills.append(skill)
-            all_skills.extend(skills)
-        elif len(split_line) == 2:
-            name, keywords = split_line
-            keywords_split = RE_SKILL_KEYWORD_DELIM.split(keywords)
-            if len(keywords_split) == 1:
-                keywords_split = keywords.split()
-            skill = {
-                "name": RE_SKILL_CLEAN.sub("", name.strip()),
-                "keywords": [RE_SKILL_CLEAN.sub("", kw) for kw in keywords_split]
-            }
-            all_skills.append(skill)
-    return all_skills
-
-
 #######################
 ## CRF-BASED PARSING ##
 #######################
@@ -112,8 +53,12 @@ FIELD_SEP_CHARS = {":", "-", "–", "(", ")"}
 ITEM_SEP_CHARS = {",", ";", "/", "&"}
 
 
-def parse_skills_section_crf(lines):
+def parse_skills_section(lines):
     """
+    Parse a sequence of text lines belonging to the "skills" section of a résumé
+    to produce structured data in the form of :class:`schemas.ResumeSkillSchema`
+    using a trained Conditional Random Field (CRF) tagger.
+
     Args:
         lines (List[str])
 
@@ -353,3 +298,67 @@ def tag(tokens, features):
     """
     tags = TAGGER.tag(features)
     return list(zip(tokens, tags))
+
+
+#########################
+## RULES-BASED PARSING ##
+#########################
+
+RE_SKILL_NAME_DELIM = re.compile(r"(?:: +| +[-–] +)")
+RE_SKILL_KEYWORD_DELIM = re.compile(r"[,;] +(?:(?:and|&) +)?")
+RE_SKILL_LEVEL = re.compile(
+    r"\(?(?P<level>advanced|intermediate|beginner|basic|proficient|exposed)\)? ?(in )?",
+    flags=re.UNICODE | re.IGNORECASE,
+)
+RE_SKILL_CLEAN = re.compile(r"[.,;]$")
+
+
+def parse_skills_section_alt(lines):
+    """
+    Parse a sequence of text lines belonging to the "skills" section of a résumé
+    to produce structured data in the form of :class:`schemas.ResumeSkillSchema`
+    using logical rules based on regular expressions.
+
+    Args:
+        lines (List[str])
+
+    Returns:
+        List[Dict[str, str]]
+
+    Note:
+        This function is not as flexible as :func:`parse_skills_section()`,
+        but its errors are probably more consistent and easier to understand.
+    """
+    all_skills = []
+    for line in lines:
+        if not line:
+            continue
+        line = line.lstrip("- ")
+        split_line = RE_SKILL_NAME_DELIM.split(line)
+        if len(split_line) == 1:
+            names = RE_SKILL_KEYWORD_DELIM.split(line)
+            if len(names) == 1:
+                names = line.split()
+            skills = []
+            for name in names:
+                match = RE_SKILL_LEVEL.search(name)
+                if match:
+                    skill = {
+                        "name": RE_SKILL_CLEAN.sub("", RE_SKILL_LEVEL.sub("", name).strip()),
+                        "level": match.group("level"),
+                    }
+                else:
+                    skill = {"name": RE_SKILL_CLEAN.sub("", name.strip())}
+                skills.append(skill)
+            all_skills.extend(skills)
+        elif len(split_line) == 2:
+            name, keywords = split_line
+            keywords_split = RE_SKILL_KEYWORD_DELIM.split(keywords)
+            if len(keywords_split) == 1:
+                keywords_split = keywords.split()
+            skill = {
+                "name": RE_SKILL_CLEAN.sub("", name.strip()),
+                "keywords": [RE_SKILL_CLEAN.sub("", kw) for kw in keywords_split]
+            }
+            all_skills.append(skill)
+    return all_skills
