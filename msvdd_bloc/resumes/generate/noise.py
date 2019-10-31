ceca83f7f4c4d84f4f5d78c61a67694639237305
Burton DeWilde <burtdewilde@gmail.com>
2019-10-30 21:36:35 -0500

    Add module for generic data noise generators

---
+++ msvdd_bloc/resumes/generate/noise.py
@@ -0,0 +1,37 @@
+import random
+
+
+def uppercase_token_text(target_label, tok_labels):
+    """
+    Change token text to uppercase for those whose label is in ``target_label``.
+
+    Args:
+        target_label (str or Set[str])
+        tok_labels (List[Tuple[str, str]])
+
+    Returns:
+        List[Tuple[str, str]]
+    """
+    if isinstance(target_label, str):
+        target_label = {target_label}
+    return [
+        (tok.upper(), label) if label in target_label else (tok, label)
+        for tok, label in tok_labels
+    ]
+
+
+def delete_token_chars(prob, tok_labels):
+    """
+    Randomly delete any given character in token texts with a probability of ``prob``.
+
+    Args:
+        prob (float)
+        tok_labels (List[Tuple[str, str]])
+
+    Returns:
+        List[Tuple[str, str]]
+    """
+    return [
+        ("".join(c for c in tok if random.random() > prob), label)
+        for tok, label in tok_labels
+    ]
