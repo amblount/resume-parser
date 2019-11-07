ce6a465d5a0d7609c003ad3bebeeef0ac5339f4e
Burton DeWilde <burtdewilde@gmail.com>
2019-11-06 21:00:45 -0600

    Rename skills.generate objs for clarity

--- msvdd_bloc/resumes/skills/generate.py
+++ msvdd_bloc/resumes/skills/generate.py
@@ -1,16 +1,13 @@
-import random
-from random import randint
+import random as rnd
 
-import faker
+from msvdd_bloc.resumes.generate_utils import FAKER
 
-
-FAKER = faker.Faker(locale="en_US")
 
 _SKILL_GROUP_SEPS = (":", "-", "")
 _ITEM_SEPS = (",", ";")
 _PROFICIENCY_LEVEL_PREPS = ("in", "with", "to", "of")
 
-_PROFICIENCY_LEVELS = (
+_LEVELS = (
     "advanced", "intermediate", "beginner",
     "experienced", "proficient", "exposed", "exposure",
     "basic", "familiar", "fluent", "native",
@@ -83,106 +80,102 @@ _HOBBIES = (
 )
 
 
-def generate_and():
-    return random.choices(["and", "&"], weights=[1.0, 0.2], k=1)[0]
+def generate_field_and():
+    return rnd.choices(["and", "&"], weights=[1.0, 0.2], k=1)[0]
 
 
-def generate_bracket_left():
+def generate_field_bracket_left():
     return "("
 
 
-def generate_bracket_right():
+def generate_field_bracket_right():
     return ")"
 
 
-def generate_database():
-    return random.choice(_DATABASES)
+def generate_field_database():
+    return rnd.choice(_DATABASES)
 
 
-def generate_dev_mix():
-    return random.choice(_DEV_MIX)
+def generate_field_dev_mix():
+    return rnd.choice(_DEV_MIX)
 
 
-def generate_hobby():
-    return random.choice(_HOBBIES)
+def generate_field_hobby():
+    return rnd.choice(_HOBBIES)
 
 
-def generate_human_language():
-    return random.choice(_HUMAN_LANGUAGES)
+def generate_field_human_language():
+    return rnd.choice(_HUMAN_LANGUAGES)
 
 
-def generate_item_sep():
+def generate_field_item_sep():
     return "{sep}{ws}".format(
-        sep=random.choices(_ITEM_SEPS, weights=[1.0, 0.2], k=1)[0],
-        ws=" " * randint(1, 2),
+        sep=rnd.choices(_ITEM_SEPS, weights=[1.0, 0.2], k=1)[0],
+        ws=" " * rnd.randint(1, 2),
     )
 
 
-def generate_newline():
-    if random.random() < 0.8:
+def generate_field_newline():
+    if rnd.random() < 0.8:
         return "\n"
     else:
         return "\n\n"
 
 
-def generate_proficiency_level():
-    return random.choice(_PROFICIENCY_LEVELS)
+def generate_field_level():
+    return rnd.choice(_LEVELS)
 
 
-def generate_proficiency_level_prep():
-    return random.choices(_PROFICIENCY_LEVEL_PREPS, weights=[1.0, 0.4, 0.2, 0.1], k=1)[0]
+def generate_field_level_prep():
+    return rnd.choices(_PROFICIENCY_LEVEL_PREPS, weights=[1.0, 0.4, 0.2, 0.1], k=1)[0]
 
 
-def generate_programming_language():
-    return random.choice(_PROGRAMMING_LANGUAGES)
+def generate_field_programming_language():
+    return rnd.choice(_PROGRAMMING_LANGUAGES)
 
 
-def generate_sentence():
-    return FAKER.sentence(nb_words=random.randint(5, 15))
+def generate_field_sentence():
+    return FAKER.sentence(nb_words=rnd.randint(5, 15))
 
 
-def generate_skill_group_name():
-    return random.choice(_SKILL_GROUP_NAMES)
+def generate_field_skill_group_name():
+    return rnd.choice(_SKILL_GROUP_NAMES)
 
 
-def generate_skill_group_sep():
+def generate_field_skill_group_sep():
     return "{ws1}{sep}".format(
-        ws1="" if random.random() < 0.9 else " ",
-        sep=random.choices(
-            _SKILL_GROUP_SEPS,
-            weights=[1.0, 0.5, 0.1],
-            k=1,
-        )[0],
+        ws1="" if rnd.random() < 0.9 else " ",
+        sep=rnd.choices(_SKILL_GROUP_SEPS, weights=[1.0, 0.5, 0.1], k=1)[0],
     )
 
 
-def generate_software():
-    return random.choice(_SOFTWARE)
+def generate_field_software():
+    return rnd.choice(_SOFTWARE)
 
 
-def generate_whitespace():
-    return " " * randint(0, 3)
+def generate_field_whitespace():
+    return " " * rnd.randint(0, 3)
 
 
 FIELDS = {
-    "and": (generate_and, "item_sep"),
+    "and": (generate_field_and, "item_sep"),
     "bullet": (lambda : "- ", "bullet"),
-    "db": (generate_database, "name"),
-    "dev_mix": (generate_dev_mix, "name"),
-    "in": (generate_proficiency_level_prep, "field_sep"),
-    "is": (generate_item_sep, "item_sep"),
-    "grp_name": (generate_skill_group_name, "name"),
-    "grp_name_sep": (generate_skill_group_sep, "field_sep"),
-    "hobby": (generate_hobby, "other"),
-    "lang": (generate_human_language, "name"),  # TODO: should this be "other"?
-    "level": (generate_proficiency_level, "level"),
-    "lb": (generate_bracket_left, "field_sep"),
-    "nl": (generate_newline, "field_sep"),
-    "plang": (generate_programming_language, "name"),
-    "rb": (generate_bracket_right, "field_sep"),
-    "sent": (generate_sentence, "other"),
-    "sw": (generate_software, "name"),
-    "ws": (generate_whitespace, "field_sep"),
+    "db": (generate_field_database, "name"),
+    "dev_mix": (generate_field_dev_mix, "name"),
+    "in": (generate_field_level_prep, "field_sep"),
+    "is": (generate_field_item_sep, "item_sep"),
+    "grp_name": (generate_field_skill_group_name, "name"),
+    "grp_name_sep": (generate_field_skill_group_sep, "field_sep"),
+    "hobby": (generate_field_hobby, "other"),
+    "lang": (generate_field_human_language, "name"),  # TODO: should this be "other"?
+    "level": (generate_field_level, "level"),
+    "lb": (generate_field_bracket_left, "field_sep"),
+    "nl": (generate_field_newline, "field_sep"),
+    "plang": (generate_field_programming_language, "name"),
+    "rb": (generate_field_bracket_right, "field_sep"),
+    "sent": (generate_field_sentence, "other"),
+    "sw": (generate_field_software, "name"),
+    "ws": (generate_field_whitespace, "field_sep"),
 }
 """
 Dict[str, Tuple[Callable, str]]: Mapping of field key string to a function that generates
@@ -191,36 +184,36 @@ a random field value and the default field label assigned to the value.
 
 _LNTMPLTS = {
     "grp_name_and_sep": lambda: "{grp_name} {grp_name_sep}",
-    "dev_mixes": lambda: " {is} ".join("{dev_mix}" for _ in range(randint(3, 9))) + " {is} {and::0.1} {dev_mix}",
-    "dev_mixes_bulleted": lambda: "{bullet} " + " {is} ".join("{dev_mix}" for _ in range(randint(3, 9))) + " {is} {and::0.1} {dev_mix}",
-    "dev_mixes_grped": lambda: "{grp_name} {grp_name_sep} {ws} " + " {is} ".join("{dev_mix:keyword}" for _ in range(randint(3, 9))) + " {is} {and::0.1} {dev_mix:keyword}",
-    "dev_mixes_kw": lambda: " {is} ".join("{dev_mix:keyword}" for _ in range(randint(3, 10))),
-    "dev_mixes_level": lambda: " {is} ".join("{dev_mix} {lb} {level} {rb}" for _ in range(randint(3, 8))),
-    "dev_mixes_level_grped": lambda: "{level} {in::0.5} {grp_name_sep::0.25}" + " {is} ".join("{dev_mix}" for _ in range(randint(3, 8))),
-    "plangs": lambda: " {is} ".join("{plang}" for _ in range(randint(3, 8))),
-    "plangs_bulleted": lambda: "{bullet} " + " {is} ".join("{plang}" for _ in range(randint(3, 8))),
-    "plangs_grped": lambda: "{grp_name} {grp_name_sep} {ws} " + " {is} ".join("{plang:keyword}" for _ in range(randint(3, 8))),
-    "plangs_kw": lambda: " {is} ".join("{plang:keyword}" for _ in range(randint(3, 8))),
-    "dbs": lambda: " {is} ".join("{db}" for _ in range(randint(3, 6))),
-    "dbs_bulleted": lambda: "{bullet} " + " {is} ".join("{db}" for _ in range(randint(3, 6))),
-    "dbs_grped": lambda: "{grp_name} {grp_name_sep} {ws} " + " {is} ".join("{db:keyword}" for _ in range(randint(3, 6))),
-    "dbs_kw": lambda: " {is} ".join("{db:keyword}" for _ in range(randint(3, 6))),
-    "langs": lambda: " {is} ".join("{lang}" for _ in range(randint(2, 4))),
-    "langs_bulleted": lambda: "{bullet} " + " {is} ".join("{lang}" for _ in range(randint(2, 4))),
-    "langs_grped": lambda: "{grp_name} {grp_name_sep} {ws} " + " {is} ".join("{lang:keyword}" for _ in range(randint(2, 4))),
-    "langs_kw": lambda: " {is} ".join("{lang:keyword}" for _ in range(randint(2, 4))),
+    "dev_mixes": lambda: " {is} ".join("{dev_mix}" for _ in range(rnd.randint(3, 9))) + " {is} {and::0.1} {dev_mix}",
+    "dev_mixes_bulleted": lambda: "{bullet} " + " {is} ".join("{dev_mix}" for _ in range(rnd.randint(3, 9))) + " {is} {and::0.1} {dev_mix}",
+    "dev_mixes_grped": lambda: "{grp_name} {grp_name_sep} {ws} " + " {is} ".join("{dev_mix:keyword}" for _ in range(rnd.randint(3, 9))) + " {is} {and::0.1} {dev_mix:keyword}",
+    "dev_mixes_kw": lambda: " {is} ".join("{dev_mix:keyword}" for _ in range(rnd.randint(3, 10))),
+    "dev_mixes_level": lambda: " {is} ".join("{dev_mix} {lb} {level} {rb}" for _ in range(rnd.randint(3, 8))),
+    "dev_mixes_level_grped": lambda: "{level} {in::0.5} {grp_name_sep::0.25}" + " {is} ".join("{dev_mix}" for _ in range(rnd.randint(3, 8))),
+    "plangs": lambda: " {is} ".join("{plang}" for _ in range(rnd.randint(3, 8))),
+    "plangs_bulleted": lambda: "{bullet} " + " {is} ".join("{plang}" for _ in range(rnd.randint(3, 8))),
+    "plangs_grped": lambda: "{grp_name} {grp_name_sep} {ws} " + " {is} ".join("{plang:keyword}" for _ in range(rnd.randint(3, 8))),
+    "plangs_kw": lambda: " {is} ".join("{plang:keyword}" for _ in range(rnd.randint(3, 8))),
+    "dbs": lambda: " {is} ".join("{db}" for _ in range(rnd.randint(3, 6))),
+    "dbs_bulleted": lambda: "{bullet} " + " {is} ".join("{db}" for _ in range(rnd.randint(3, 6))),
+    "dbs_grped": lambda: "{grp_name} {grp_name_sep} {ws} " + " {is} ".join("{db:keyword}" for _ in range(rnd.randint(3, 6))),
+    "dbs_kw": lambda: " {is} ".join("{db:keyword}" for _ in range(rnd.randint(3, 6))),
+    "langs": lambda: " {is} ".join("{lang}" for _ in range(rnd.randint(2, 4))),
+    "langs_bulleted": lambda: "{bullet} " + " {is} ".join("{lang}" for _ in range(rnd.randint(2, 4))),
+    "langs_grped": lambda: "{grp_name} {grp_name_sep} {ws} " + " {is} ".join("{lang:keyword}" for _ in range(rnd.randint(2, 4))),
+    "langs_kw": lambda: " {is} ".join("{lang:keyword}" for _ in range(rnd.randint(2, 4))),
 }
 
 TEMPLATES = [
-    lambda: " {nl} ".join(_LNTMPLTS["dev_mixes"]() for _ in range(randint(1, 4))),
-    lambda: " {nl} ".join(_LNTMPLTS["dev_mixes_bulleted"]() for _ in range(randint(1, 4))),
-    lambda: " {nl} ".join(_LNTMPLTS["dev_mixes_grped"]() for _ in range(randint(1, 4))),
-    lambda: " {nl} ".join(_LNTMPLTS["dev_mixes_level"]() for _ in range(randint(1, 2))),
-    lambda: " {nl} ".join(_LNTMPLTS["dev_mixes_level_grped"]() for _ in range(randint(1, 3))),
+    lambda: " {nl} ".join(_LNTMPLTS["dev_mixes"]() for _ in range(rnd.randint(1, 4))),
+    lambda: " {nl} ".join(_LNTMPLTS["dev_mixes_bulleted"]() for _ in range(rnd.randint(1, 4))),
+    lambda: " {nl} ".join(_LNTMPLTS["dev_mixes_grped"]() for _ in range(rnd.randint(1, 4))),
+    lambda: " {nl} ".join(_LNTMPLTS["dev_mixes_level"]() for _ in range(rnd.randint(1, 2))),
+    lambda: " {nl} ".join(_LNTMPLTS["dev_mixes_level_grped"]() for _ in range(rnd.randint(1, 3))),
     lambda: " {nl} ".join([_LNTMPLTS["plangs_bulleted"](), _LNTMPLTS["dbs_bulleted"](), _LNTMPLTS["dev_mixes_bulleted"]()]),
     lambda: " {nl} ".join([_LNTMPLTS["plangs_grped"](), _LNTMPLTS["dbs_grped"]()]),
     lambda: " {nl} ".join([_LNTMPLTS["grp_name_and_sep"](), _LNTMPLTS["dev_mixes"](), _LNTMPLTS["grp_name_and_sep"](), _LNTMPLTS["dev_mixes"]()]),
-    lambda: " {nl} ".join(" {nl} ".join("{dev_mix}" for _ in range(randint(3, 10))) for _ in range(randint(1, 2))),
+    lambda: " {nl} ".join(" {nl} ".join("{dev_mix}" for _ in range(rnd.randint(3, 10))) for _ in range(rnd.randint(1, 2))),
     lambda: "{bullet::0.25} " + _LNTMPLTS["dev_mixes_level_grped"]() + " {grp_name_sep} " + _LNTMPLTS["dev_mixes_level_grped"](),
-    lambda: "{bullet} " + _LNTMPLTS["dev_mixes_grped"]() + " {nl} ".join("{bullet} {sent}" for i in range(randint(1, 3))),
+    lambda: "{bullet} " + _LNTMPLTS["dev_mixes_grped"]() + " {nl} ".join("{bullet} {sent}" for i in range(rnd.randint(1, 3))),
 ]
