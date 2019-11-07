217d85f5a07af24cab29bfd7e2733fba4d436c9d
Burton DeWilde <burtdewilde@gmail.com>
2019-11-06 22:27:13 -0600

    Add docs, tidy up names in skills gen

--- msvdd_bloc/resumes/skills/generate.py
+++ msvdd_bloc/resumes/skills/generate.py
@@ -4,7 +4,7 @@ import random as rnd
 from msvdd_bloc.resumes.generate_utils import FAKER
 
 
-_SKILL_GROUP_SEPS = (":", "-", "")
+_GROUP_SEPS = (":", "-", "")
 _ITEM_SEPS = (",", ";")
 _PROFICIENCY_LEVEL_PREPS = ("in", "with", "to", "of")
 
@@ -88,20 +88,19 @@ def generate_field_and():
     return rnd.choices(["and", "&"], weights=[1.0, 0.2], k=1)[0]
 
 
-def generate_field_bracket_left():
-    return "("
-
-
-def generate_field_bracket_right():
-    return ")"
-
-
 def generate_field_database():
     return rnd.choice(_DATABASES)
 
 
 def generate_field_dev_mix():
     return rnd.choice(_DEV_MIX)
+
+
+def generate_field_group_sep():
+    return "{ws1}{sep}".format(
+        ws1="" if rnd.random() < 0.9 else " ",
+        sep=rnd.choices(_GROUP_SEPS, weights=[1.0, 0.5, 0.1], k=1)[0],
+    )
 
 
 def generate_field_hobby():
@@ -146,13 +145,6 @@ def generate_field_skill_group_name():
     return rnd.choice(_SKILL_GROUP_NAMES)
 
 
-def generate_field_skill_group_sep():
-    return "{ws1}{sep}".format(
-        ws1="" if rnd.random() < 0.9 else " ",
-        sep=rnd.choices(_SKILL_GROUP_SEPS, weights=[1.0, 0.5, 0.1], k=1)[0],
-    )
-
-
 def generate_field_software():
     return rnd.choice(_SOFTWARE)
 
@@ -168,15 +160,15 @@ FIELDS = {
     "dev_mix": (generate_field_dev_mix, "name"),
     "is": (generate_field_item_sep, "item_sep"),
     "grp_name": (generate_field_skill_group_name, "name"),
-    "grp_name_sep": (generate_field_skill_group_sep, "field_sep"),
+    "grp_sep": (generate_field_group_sep, "field_sep"),
     "hobby": (generate_field_hobby, "other"),
     "lang": (generate_field_human_language, "name"),  # TODO: should this be "other"?
     "level": (generate_field_level, "level"),
     "level_prep": (generate_field_level_prep, "field_sep"),
-    "lb": (generate_field_bracket_left, "field_sep"),
+    "lb": (lambda: "(", "field_sep"),
     "nl": (generate_field_newline, "field_sep"),
     "plang": (generate_field_programming_language, "name"),
-    "rb": (generate_field_bracket_right, "field_sep"),
+    "rb": (lambda: ")", "field_sep"),
     "sent": (generate_field_sentence, "other"),
     "sw": (generate_field_software, "name"),
     "ws": (generate_field_whitespace, "field_sep"),
@@ -192,6 +184,9 @@ a random field value and the default field label assigned to the value.
 
 def generate_line_fields(*, key, nrange, bullet=False):
     """
+    Generate line template for a list of fields, formatted as
+    ``{key} {is} {key} {is} {and::0.1} {key}``.
+
     Args:
         key (str)
         nrange (Tuple[int, int])
@@ -208,11 +203,14 @@ def generate_line_fields(*, key, nrange, bullet=False):
             for _ in range(rnd.randint(nmin - 1, nmax - 1))
         ),
         field_end="{{is}} {{and::0.1}} {{{key}}}".format(key=key),
-    )
+    ).strip()
 
 
 def generate_line_fields_grped(*, key, nrange, bullet=False):
     """
+    Generate line template for a list of fields with a group name, formatted as
+    ``{grp_name} {grp_sep} {ws} {key:keyword} {is} {key:keyword} {is} {and::0.1} {key:keyword}``.
+
     Args:
         key (str)
         nrange (Tuple[int, int])
@@ -224,17 +222,20 @@ def generate_line_fields_grped(*, key, nrange, bullet=False):
     nmin, nmax = nrange
     return "{blt} {grp} {fields} {field_end}".format(
         blt="" if bullet is False else "{bullet}",
-        grp="{grp_name} {grp_name_sep} {ws}",
+        grp="{grp_name} {grp_sep} {ws}",
         fields=" {is} ".join(
             "{{{key}:keyword}}".format(key=key)
             for _ in range(rnd.randint(nmin - 1, nmax - 1))
         ),
         field_end="{{is}} {{and::0.1}} {{{key}:keyword}}".format(key=key),
-    )
+    ).strip()
 
 
 def generate_line_fields_levels(*, key, nrange, bullet=False):
     """
+    Generate line template for a list of fields with individual levels, formatted as
+    ``{key} {lb} {level} {rb} {is} {key} {lb} {level} {rb} {is} {and::0.1} {key} {lb} {level} {rb}``.
+
     Args:
         key (str)
         nrange (Tuple[int, int])
@@ -251,11 +252,14 @@ def generate_line_fields_levels(*, key, nrange, bullet=False):
             for _ in range(rnd.randint(nmin - 1, nmax - 1))
         ),
         field_end="{{is}} {{and::0.1}} {{{key}}} {{lb}} {{level}} {{rb}}".format(key=key),
-    )
+    ).strip()
 
 
 def generate_line_fields_level_grped(*, key, nrange, bullet=False):
     """
+    Generate line template for a list of fields with a group-wide level, formatted as
+    ``{level} {level_prep::0.5} {grp_sep::0.25} {key} {is} {key} {is} {and::0.1} {key}``.
+
     Args:
         key (str)
         nrange (Tuple[int, int])
@@ -267,17 +271,17 @@ def generate_line_fields_level_grped(*, key, nrange, bullet=False):
     nmin, nmax = nrange
     return "{blt} {lvl} {fields} {field_end}".format(
         blt="" if bullet is False else "{bullet}",
-        lvl="{level} {level_prep::0.5} {grp_name_sep::0.25}",
+        lvl="{level} {level_prep::0.5} {grp_sep::0.25}",
         fields=" {is} ".join(
             "{{{key}}}".format(key=key)
             for _ in range(rnd.randint(nmin - 1, nmax - 1))
         ),
         field_end="{{is}} {{and::0.1}} {{{key}}}".format(key=key),
-    )
+    ).strip()
 
 
 _LINES = {
-    "grp_name_and_sep": lambda: "{grp_name} {grp_name_sep}",
+    "grp_name_and_sep": lambda: "{grp_name} {grp_sep}",
     "dev_mixes": fnc.partial(generate_line_fields, key="dev_mix", nrange=(3, 10)),
     "dev_mixes_bulleted": fnc.partial(generate_line_fields, key="dev_mix", nrange=(3, 10), bullet=True),
     "dev_mixes_grped": fnc.partial(generate_line_fields_grped, key="dev_mix", nrange=(3, 10)),
@@ -305,8 +309,8 @@ TEMPLATES = [
     lambda: " {nl} ".join([_LINES["plangs_grped"](), _LINES["dbs_grped"]()]),
     lambda: " {nl} ".join([_LINES["langs_grped"](), _LINES["plangs_grped"]()]),
     lambda: " {nl} ".join([_LINES["grp_name_and_sep"](), _LINES["dev_mixes"](), _LINES["grp_name_and_sep"](), _LINES["dev_mixes"]()]),
-    lambda: " {nl} ".join(["{level} {grp_name_sep::0.5}", _LINES["dev_mixes"](), "{level} {grp_name_sep::0.5}", _LINES["dev_mixes"]()]),
+    lambda: " {nl} ".join(["{level} {grp_sep::0.5}", _LINES["dev_mixes"](), "{level} {grp_sep::0.5}", _LINES["dev_mixes"]()]),
     lambda: " {nl} ".join(" {nl} ".join("{dev_mix}" for _ in range(rnd.randint(3, 10))) for _ in range(rnd.randint(1, 2))),
-    lambda: "{bullet::0.25} " + _LINES["dev_mixes_level_grped"]() + " {grp_name_sep} " + _LINES["dev_mixes_level_grped"](),
+    lambda: "{bullet::0.25} " + _LINES["dev_mixes_level_grped"]() + " {grp_sep} " + _LINES["dev_mixes_level_grped"](),
     lambda: "{bullet} " + _LINES["dev_mixes_grped"]() + " {nl} ".join("{bullet} {sent}" for i in range(rnd.randint(1, 3))),
 ]
