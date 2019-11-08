cfbfd7b73fcb94275d10b2879b6cab338273afac
Burton DeWilde <burtdewilde@gmail.com>
2019-11-07 22:15:54 -0600

    Better randomize generated basics sections
    and tidy/refactor code to be more like skills generator module

--- msvdd_bloc/resumes/basics/generate.py
+++ msvdd_bloc/resumes/basics/generate.py
@@ -1,5 +1,5 @@
-import functools
-import random
+import functools as fnc
+import random as rnd
 
 from msvdd_bloc.resumes import noise_utils
 from msvdd_bloc.resumes.generate_utils import FAKER
@@ -19,6 +19,9 @@ _PHONE_FORMATS = (
 )
 _URL_SCHEMES = ("www.", "http://", "http://www.", "")
 
+#############################
+## random field generators ##
+#############################
 
 def generate_field_address():
     return FAKER.address().replace("\n", " ")
@@ -49,8 +52,8 @@ def generate_field_email():
 
 def generate_field_field_sep():
     return "{ws}{sep}{ws}".format(
-        ws=" " * random.randint(1, 4),
-        sep=random.choices(
+        ws=" " * rnd.randint(1, 4),
+        sep=rnd.choices(
             _FIELD_SEPS,
             weights=[1.0, 0.5, 0.5, 0.25, 0.1, 0.05, 0.05, 0.05, 0.05],
             k=1,
@@ -60,40 +63,40 @@ def generate_field_field_sep():
 
 def generate_field_item_sep():
     return "{sep}{ws}".format(
-        sep=random.choices(_ITEM_SEPS, weights=[1.0, 0.2], k=1)[0],
-        ws=" " * random.randint(1, 2),
+        sep=rnd.choices(_ITEM_SEPS, weights=[1.0, 0.2], k=1)[0],
+        ws=" " * rnd.randint(1, 2),
     )
 
 
 def generate_field_label_addr():
     return "{label}{ws}{sep}".format(
-        label=random.choices(_FIELD_LABEL_ADDRS , weights=[1.0, 0.1], k=1)[0],
-        ws="" if random.random() < 0.9 else " ",
-        sep=random.choices(_FIELD_LABEL_SEPS, weights=[1.0, 0.2, 0.2], k=1)[0],
+        label=rnd.choices(_FIELD_LABEL_ADDRS , weights=[1.0, 0.1], k=1)[0],
+        ws="" if rnd.random() < 0.9 else " ",
+        sep=rnd.choices(_FIELD_LABEL_SEPS, weights=[1.0, 0.2, 0.2], k=1)[0],
     )
 
 
 def generate_field_label_email():
     return "{label}{ws}{sep}".format(
-        label=random.choice(_FIELD_LABEL_EMAILS),
-        ws="" if random.random() < 0.9 else " ",
-        sep=random.choices(_FIELD_LABEL_SEPS, weights=[1.0, 0.2, 0.2], k=1)[0],
+        label=rnd.choice(_FIELD_LABEL_EMAILS),
+        ws="" if rnd.random() < 0.9 else " ",
+        sep=rnd.choices(_FIELD_LABEL_SEPS, weights=[1.0, 0.2, 0.2], k=1)[0],
     )
 
 
 def generate_field_label_phone():
     return "{label}{ws}{sep}".format(
-        label=random.choice(_FIELD_LABEL_PHONES),
-        ws="" if random.random() < 0.9 else " ",
-        sep=random.choices(_FIELD_LABEL_SEPS, weights=[1.0, 0.2, 0.2], k=1)[0],
+        label=rnd.choice(_FIELD_LABEL_PHONES),
+        ws="" if rnd.random() < 0.9 else " ",
+        sep=rnd.choices(_FIELD_LABEL_SEPS, weights=[1.0, 0.2, 0.2], k=1)[0],
     )
 
 
 def generate_field_label_profile():
     return "{label}{ws}{sep}".format(
-        label=random.choice(_FIELD_LABEL_PROFILES),
-        ws="" if random.random() < 0.9 else " ",
-        sep=random.choices(_FIELD_LABEL_SEPS, weights=[1.0, 0.2, 0.2], k=1)[0],
+        label=rnd.choice(_FIELD_LABEL_PROFILES),
+        ws="" if rnd.random() < 0.9 else " ",
+        sep=rnd.choices(_FIELD_LABEL_SEPS, weights=[1.0, 0.2, 0.2], k=1)[0],
     )
 
 
@@ -102,59 +105,59 @@ def generate_field_name():
 
 
 def generate_field_newline():
-    if random.random() < 0.8:
+    if rnd.random() < 0.8:
         return "\n"
     else:
         return "\n\n"
 
 
 def generate_field_phone():
-    if random.random() < 0.25:
+    if rnd.random() < 0.25:
         return FAKER.phone_number()
     else:
-        return FAKER.numerify(random.choice(_PHONE_FORMATS))
+        return FAKER.numerify(rnd.choice(_PHONE_FORMATS))
 
 
 def generate_field_resume_label():
-    if random.random() > 0.8:
+    if rnd.random() > 0.8:
         return FAKER.job()
     else:
         return FAKER.catch_phrase()
 
 
 def generate_field_sentence():
-    return FAKER.sentence(nb_words=random.randint(5, 15))
+    return FAKER.sentence(nb_words=rnd.randint(5, 15))
 
 
 def generate_field_user_name():
     return "{at}{name}".format(
-        at="@" if random.random() < 0.33 else "",
+        at="@" if rnd.random() < 0.33 else "",
         name=FAKER.user_name(),
     )
 
 
 def generate_field_website():
-    if random.random() < 0.5:
+    if rnd.random() < 0.5:
         return FAKER.url()
     else:
-        return FAKER.domain_name(levels=random.randint(1, 2))
+        return FAKER.domain_name(levels=rnd.randint(1, 2))
 
 
 def generate_field_website_profile():
-    if random.random() < 0.5:
+    if rnd.random() < 0.5:
         return "{scheme}linkedin.com/in/{slug}".format(
-            scheme=random.choice(_URL_SCHEMES),
+            scheme=rnd.choice(_URL_SCHEMES),
             slug=FAKER.slug(value=FAKER.name()),
         )
     else:
         return "{scheme}github.com/{user}".format(
-            scheme=random.choice(_URL_SCHEMES),
+            scheme=rnd.choice(_URL_SCHEMES),
             user=FAKER.user_name(),
         )
 
 
 def generate_field_whitespace():
-    return " " * random.randint(1, 4)
+    return " " * rnd.randint(1, 4)
 
 
 FIELDS = {
@@ -173,10 +176,10 @@ FIELDS = {
     "name": (generate_field_name, "name"),
     "nl": (generate_field_newline, "field_sep"),
     "phone": (generate_field_phone, "phone"),
-    "sent_short": (generate_field_sentence, "other"),
+    "profile": (generate_field_website_profile, "website"),  # TODO: improve this?
+    "sent": (generate_field_sentence, "other"),
     "user_name": (generate_field_user_name, "profile"),
     "website": (generate_field_website, "website"),
-    "website_profile": (generate_field_website_profile, "website"),  # TODO: improve this?
     "ws": (generate_field_whitespace, "field_sep"),
 }
 """
@@ -184,54 +187,130 @@ Dict[str, Tuple[Callable, str]]: Mapping of field key string to a function that 
 a random field value and the default field label assigned to the value.
 """
 
+############################
+## random line generators ##
+############################
+
+def generate_line_fields_shuffle(*, field_keys, sep_key, n):
+    """
+    Args:
+        field_keys (List[str])
+        sep_key (str)
+        n (int)
+
+    Returns:
+        str
+    """
+    sep = " {{{key}}} ".format(key=sep_key)
+    return sep.join(
+        "{{{key}}}".format(key=key)
+        for key in rnd.sample(field_keys, min(n, len(field_keys)))
+    )
+
+
+def generate_line_fields_labeled_shuffle(*, field_keys, sep_key, n):
+    """
+    Args:
+        field_keys (List[str])
+        sep_key (str)
+        n (int)
+
+    Returns:
+        str
+    """
+    sep = " {{{key}}} ".format(key=sep_key)
+    return sep.join(
+        "{{label_{key}}} {{ws}} {{{key}}}".format(key=key)
+        for key in rnd.sample(field_keys, min(n, len(field_keys)))
+    )
+
+
 TEMPLATES = [
-    "{addr_city_state} {nl} {phone} {nl} {name} {nl} {website_profile} {nl} {email} {nl} {website_profile}",
-    "{addr_street} {nl} {addr_city_state_zip} {nl} {name} {nl} {phone} {nl} {email} {nl} {website}",
-    "{name}",
-    "{name} {fs} {email} {fs} {website_profile}",
-    "{name} {fs} {email} {nl} {website} {fs} {phone} {nl} {user_name} {fs:item_sep} {user_name}",
-    "{name} {nl} {addr} {fs} {email} {fs} {phone}",
-    "{name} {nl} {addr} {fs} {phone} {fs} {email} {fs} {nl} {website_profile}",
-    "{name} {nl} {addr} {nl} {phone} {nl} {email}",
-    "{name} {nl} {addr} {nl} {phone} {nl} {email} {fs} {website_profile}",
-    "{name} {nl} {addr} {ws} {phone} {ws} {email}",
-    "{name} {nl} {addr_city_state} {fs} {phone} {fs} {email}",
-    "{name} {nl} {addr_city_state} {nl} {email} {nl} {website_profile}",
-    "{name} {nl} {addr_street} {fs:item_sep} {addr_city_state_zip} {fs} {email} {fs} {phone}",
-    "{name} {nl} {addr_street} {fs:item_sep} {addr_city_state_zip} {fs} {phone} {fs} {email} {nl} {website_profile} {fs:item_sep} {website_profile}",
-    "{name} {nl} {email}",
-    "{name} {nl} {email} {fs} {addr}",
-    "{name} {nl} {email} {fs} {phone}",
-    "{name} {nl} {email} {fs} {phone} {fs} {addr}",
-    "{name} {nl} {email} {fs} {phone} {fs} {addr} {nl} {website} {fs} {website_profile}",
-    "{name} {nl} {email} {fs} {phone} {fs} {website_profile}",
-    "{name} {nl} {email} {fs} {phone} {fs} {website_profile} {fs:item_sep} {website_profile}",
-    "{name} {nl} {email} {fs} {phone} {nl} {sent_short}",
-    "{name} {nl} {email} {nl} {label_phone} {ws} {phone}",
-    "{name} {nl} {email} {nl} {phone} {nl} {label_addr} {ws} {addr}",
-    "{name} {nl} {email} {ws} {phone} {website} {addr_city_state}",
-    "{name} {nl} {label} {nl} {email} {fs} {phone} {fs} {addr_street} {fs:item_sep} {addr_city_state} {nl} {website} {fs} {user_name} {fs} {website_profile} {fs} {website_profile}",
-    "{name} {nl} {label} {nl} {email} {ws} {phone} {ws} {website_profile} {ws:item_sep} {website_profile}",
-    "{name} {nl} {label} {nl} {sent_short}",
-    "{name} {nl} {label_addr} {ws} {addr} {nl} {phone} {fs} {label_profile} {ws} {ws} {user_name} {fs} {email} {fs} {website_profile}",
-    "{name} {nl} {label_email} {ws} {email} {fs} {label_phone} {ws} {phone} {fs} {label_profile} {ws} {website}",
-    "{name} {nl} {label_email} {ws} {email} {fs} {label_phone} {ws} {phone} {fs} {label_profile} {ws} {website_profile}",
-    "{name} {nl} {label_phone} {ws} {phone} {fs} {label_email} {ws} {email} {fs} {label_profile} {ws} {website_profile}",
-    "{name} {nl} {label_phone} {ws} {phone} {fs} {website_profile} {fs} {label_email} {ws} {email}",
-    "{name} {nl} {label_phone} {ws} {phone} {nl} {label_email} {ws} {email} {label_profile} {ws} {website_profile} {nl} {label_profile} {ws} {website_profile}",
-    "{name} {nl} {label_phone} {ws} {phone} {ws} {label_email} {ws} {email} {nl} {label_profile} {ws} {website_profile} {ws} {label_profile} {ws} {user_name}",
-    "{name} {nl} {phone} {fs} {email} {fs} {addr}",
-    "{name} {nl} {phone} {fs} {email} {nl} {website_profile} {fs} {website_profile}",
-    "{name} {nl} {phone} {nl} {email} {nl} {website_profile} {nl} {addr}",
-    "{name} {nl} {phone} {nl} {website} {nl} {email}",
-    "{name} {nl} {phone} {ws} {email}",
-    "{name} {nl} {website} {nl} {email} {fs} {phone}",
-    "{name} {nl} {website_profile} {nl} {phone} {ws} {email}",
-    "{name} {ws} {addr} {nl} {user_name} {ws} {website_profile} {ws} {phone}",
-    "{name} {ws} {addr} {nl} {website_profile} {ws} {email} {nl} {phone}",
-    "{name} {ws} {label_email} {ws} {email} {nl} {website} {ws} {label_phone} {ws} {phone}",
-    "{name} {ws} {email} {fs} {phone} {fs} {addr_city_state} {fs} {website_profile} {fs:item_sep} {website_profile}",
-    "{name} {ws} {email} {phone} {ws} {website_profile} {ws} {addr_city_state}",
+    lambda: " {nl} ".join(
+        [
+            "{name}",
+            "{label::0.05}",
+            generate_line_fields_shuffle(
+                field_keys=["addr|addr_city_state|addr_city_state_zip", "email", "phone", "user_name", "website", "profile"],
+                sep_key="fs",
+                n=rnd.randint(1, 5),
+            ),
+            "{sent::0.05}",
+        ]
+    ),
+    lambda: " {nl} ".join(
+        [
+            "{name}",
+            "{label::0.05}",
+            generate_line_fields_shuffle(
+                field_keys=["addr|addr_city_state|addr_city_state_zip", "email", "phone", "profile", "user_name", "website"],
+                sep_key="nl",
+                n=rnd.randint(1, 5),
+            ),
+        ]
+    ),
+    lambda: " {nl} ".join(
+        [
+            "{name}",
+            "{label::0.05}",
+            generate_line_fields_shuffle(
+                field_keys=["addr|addr_city_state|addr_city_state_zip", "email", "phone", "profile", "user_name", "website"],
+                sep_key="ws",
+                n=rnd.randint(1, 5),
+            ),
+            "{sent::0.05}",
+        ]
+    ),
+    lambda: " {nl} ".join(
+        [
+            "{name}",
+            "{label::0.05}",
+            generate_line_fields_shuffle(
+                field_keys=["addr|addr_city_state|addr_city_state_zip", "phone"],
+                sep_key="fs",
+                n=2,
+            ),
+            generate_line_fields_shuffle(
+                field_keys=["email", "user_name", "website", "profile"],
+                sep_key="fs",
+                n=rnd.randint(2, 4),
+            ),
+        ]
+    ),
+    lambda: " {nl} ".join(
+        [
+            "{addr|addr_city_state|addr_city_state_zip}",
+            "{name}",
+            generate_line_fields_shuffle(
+                field_keys=["email", "profile", "user_name", "website"],
+                sep_key="fs|nl",
+                n=rnd.randint(2, 4),
+            ),
+        ]
+    ),
+    lambda: " {nl} ".join(
+        [
+            "{name}",
+            "{label::0.05}",
+            generate_line_fields_labeled_shuffle(
+                field_keys=["addr", "email", "phone", "profile"],
+                sep_key="fs",
+                n=rnd.randint(2, 4),
+            ),
+            "{sent::0.05}",
+        ]
+    ),
+    lambda: " {nl|fs} ".join(
+        [
+            "{name}",
+            "{addr_street} {fs:item_sep} {addr_city_state_zip}",
+            generate_line_fields_shuffle(
+                field_keys=["email", "phone", "profile", "website"],
+                sep_key="fs",
+                n=rnd.randint(2, 4),
+            ),
+        ]
+    ),
 ]
 
 
@@ -244,9 +323,9 @@ def add_noise(tok_labels):
         List[Tuple[str, str]]
     """
     noise_funcs = []
-    # noise_funcs.append(functools.partial(noise_utils.delete_token_chars, 0.001))
-    if random.random() < 0.05:
-        noise_funcs.append(functools.partial(noise_utils.uppercase_token_text, {"name"}))
+    # noise_funcs.append(fnc.partial(noise_utils.delete_token_chars, 0.001))
+    if rnd.random() < 0.05:
+        noise_funcs.append(fnc.partial(noise_utils.uppercase_token_text, {"name"}))
     for noise_func in noise_funcs:
         tok_labels = noise_func(tok_labels)
     return tok_labels
