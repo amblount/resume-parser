b1e1a14a74f2ac2cf3d3c17802363d40a04705f9
Burton DeWilde <burtdewilde@gmail.com>
2019-11-14 19:55:01 -0600

    Bring old script up to standard; remove old parse util

--- msvdd_bloc/resumes/parse_utils.py
+++ msvdd_bloc/resumes/parse_utils.py
@@ -106,22 +106,6 @@ def tag(tokens, features, *, tagger):
     return list(zip(tokens, tags))
 
 
-def load_module_from_path(*, name, fpath):
-    """
-    Args:
-        name (str)
-        fpath (str of :class:`pathlib.Path`)
-    """
-    if name in sys.modules:
-        return sys.modules[name]
-    else:
-        spec = importlib.util.spec_from_file_location(name, fpath)
-        module = importlib.util.module_from_spec(spec)
-        sys.modules[name] = module
-        spec.loader.exec_module(module)
-        return module
-
-
 def load_tagger(fpath):
     """
     Args:
