19dccd014ae590a86af6d0265d4ce33d830624c5
Burton DeWilde <burtdewilde@gmail.com>
2019-11-23 14:07:20 -0600

    Refactor education generate functionality
    and label bullets as field separators rather than separate classes

--- msvdd_bloc/resumes/education/generate.py
+++ msvdd_bloc/resumes/education/generate.py
@@ -7,67 +7,69 @@ import faker
 from msvdd_bloc.resumes.education import constants as c
 from msvdd_bloc.resumes import generate_utils
 
+###################################
+## random field value generators ##
 
 class Provider(generate_utils.ResumeProvider):
     """Class for providing randomly-generated field values."""
 
     _area_detail_templates = (
-        "{subunit} {field_sep} {area_of_study}",
-        "{area_of_study} {subunit}",
+        ("{subunit} {field_sep} {area_of_study}", 1.0),
+        ("{area_of_study} {subunit}", 0.25),
     )
     _city_state_templates = (
-        "{city}, {state_abbr}",
-        "{city}, {state}",
+        ("{city}, {state_abbr}", 1.0),
+        ("{city}, {state}", 0.2),
     )
     _course_templates = (
-        "{subject}",
-        "{prefix} {subject}",
-        "{subject} {suffix}",
-        "{subject} & {subject2}",
+        ("{subject}", 1.0),
+        ("{prefix} {subject}", 0.5),
+        ("{subject} {suffix}", 0.2),
+        ("{subject} & {subject2}", 0.1),
     )
     _date_approx_templates = (
-        "{month} {year}",
-        "{month_abbr} {year}",
-        "{month_abbr}. {year}",
-        "{season} {year}",
-        "{year}",
+        ("{month} {year}", 1.0),
+        ("{month_abbr} {year}", 1.0),
+        ("{month_abbr}. {year}", 0.25),
+        ("{season} {year}", 0.25),
+        ("{year}", 0.25),
     )
     _gpa_templates = (
-        "{gpa:.{prec}f}",
-        "{gpa:.{prec}f}{ws}/{ws}{max_gpa:.{prec}f}",
+        ("{gpa:.{prec}f}", 1.0),
+        ("{gpa:.{prec}f}{ws}/{ws}{max_gpa:.{prec}f}", 0.5),
     )
     _school_templates = (
-        "{school_name}",
-        "{school_name} {field_sep_sm} {city_state}",
-        "{school_name} {field_sep_dt} {city_state}",
+        ("{school_name}", 1.0),
+        ("{school_name} {field_sep_sm} {city_state}", 0.5),
+        ("{school_name} {field_sep_dt} {city_state}", 0.25),
     )
     _school_name_templates = (
         "{person_name} {school_type}",
         "{place_name} {school_type}",
     )
     _university_templates = (
-        "{university_name}",
-        "{university_name} {field_sep_sm} {city_state}",
-        "{university_name} {field_sep_dt} {city_state}",
+        ("{university_name}", 1.0),
+        ("{university_name} {field_sep_sm} {city_state}", 0.5),
+        ("{university_name} {field_sep_dt} {city_state}", 0.25),
     )
     _university_name_templates = (
-        "{person_name} {uni_type}",
-        "{place_name} {uni_type}",
-        "{uni_type} of {place_name}",
-        "{uni_type} of {place_name_full}",
-        "{uni_type} of {place_name}, {uni_subunit} of {area_of_study}",
+        ("{person_name} {uni_type}", 1.0),
+        ("{place_name} {uni_type}", 1.0),
+        ("{uni_type} of {place_name}", 1.0),
+        ("{uni_type} of {place_name_full}", 0.5),
+        ("{uni_type} of {place_name}, {uni_subunit} of {area_of_study}", 0.25),
     )
     _university_name_place_name_templates = (
-        "{state}",
-        "{city}",
-        "{direction}{ern} {state}",
+        ("{state}", 1.0),
+        ("{city}", 1.0),
+        ("{direction}{ern} {state}", 0.5),
     )
 
     def area_of_study(self):
         return rnd.choice(c.AREAS_OF_STUDY)
 
     def area_detail(self):
-        template = rnd.choices(self._area_detail_templates, weights=[1.0, 0.25], k=1)[0]
+        template = self.generator.random_template_weighted(self._area_detail_templates)
         return template.format(
             area_of_study=self.area_of_study(),
             field_sep=rnd.choice(c.FIELD_SEP_SMS + c.FIELD_SEP_PREPS + (":",)),
@@ -75,7 +77,7 @@ class Provider(generate_utils.ResumeProvider):
         )
 
     def city_state(self):
-        template = rnd.choices(self._city_state_templates, weights=[1.0, 0.2], k=1)[0]
+        template = self.generator.random_template_weighted(self._city_state_templates)
         return template.format(
             city=self.generator.city(),
             state=self.generator.state(),
@@ -83,14 +85,12 @@ class Provider(generate_utils.ResumeProvider):
         )
 
     def course_title(self):
-        template = rnd.choices(
-            self._course_templates, weights=[1.0, 0.5, 0.2, 0.1], k=1,
-        )[0]
+        template = self.generator.random_template_weighted(self._course_templates)
         # add a course code?
         if rnd.random() < 0.05:
             template += " ({dep_code} {num_code})".format(
-                dep_code=self.generator.lexify("?" * rnd.randint(2, 4)).upper(),
-                num_code=self.generator.numerify("#" * rnd.randint(2, 4)),
+                dep_code=self.generator.lexify("?" * rnd.randrange(2, 5)).upper(),
+                num_code=self.generator.numerify("#" * rnd.randrange(2, 5)),
             )
         return template.format(
             prefix=rnd.choice(c.COURSE_PREFIXES),
@@ -100,9 +100,7 @@ class Provider(generate_utils.ResumeProvider):
         )
 
     def date_approx(self):
-        template = rnd.choices(
-            self._date_approx_templates, weights=[1.0, 1.0, 0.25, 0.25, 0.25], k=1,
-        )[0]
+        template = self.generator.random_template_weighted(self._date_approx_templates)
         month = self.generator.month_name()
         return template.format(
             month=month,
@@ -112,36 +110,36 @@ class Provider(generate_utils.ResumeProvider):
         )
 
     def field_sep(self):
-        return self.generator.sep_with_ws(c.FIELD_SEPS, weights=None, ws_nrange=(1, 3))
+        return self.generator.sep_with_ws(c.FIELD_SEPS, weights=None, ws_nrange=(1, 4))
 
     def field_sep_dt(self):
         return self.generator.sep_with_ws(
-            c.FIELD_SEP_DTS, weights=[1.0, 0.5, 0.1], ws_nrange=(1, 2),
+            c.FIELD_SEP_DTS, weights=[1.0, 0.5, 0.1], ws_nrange=(1, 3),
         )
 
     def field_sep_prep(self):
         return self.generator.sep_with_ws(
-            c.FIELD_SEP_PREPS, weights=None, ws_nrange=(1, 2),
+            c.FIELD_SEP_PREPS, weights=None, ws_nrange=(1, 3),
         )
 
     def field_sep_sm(self):
         return self.generator.sep_with_ws_right(
-            c.FIELD_SEP_SMS, weights=[1.0, 0.5], ws_nrange=(1, 2),
+            c.FIELD_SEP_SMS, weights=[1.0, 0.5], ws_nrange=(1, 3),
         )
 
     def gpa(self):
-        template = rnd.choices(self._gpa_templates, weights=[1.0, 0.5], k=1)[0]
+        template = self.generator.random_template_weighted(self._gpa_templates)
         gpa, max_gpa = sorted(rnd.uniform(1.0, 4.0) for _ in range(2))
         return template.format(
             gpa=gpa,
             max_gpa=max_gpa,
-            prec=rnd.randint(1, 2),
+            prec=rnd.randrange(1, 3),
             ws=rnd.choice(["", " "]),
         )
 
     def item_sep(self):
         return self.generator.sep_with_ws_right(
-            c.FIELD_SEP_SMS, weights=[1.0, 0.25], ws_nrange=(1, 2),
+            c.FIELD_SEP_SMS, weights=[1.0, 0.25], ws_nrange=(1, 3),
         )
 
     def label_courses(self):
@@ -163,7 +161,7 @@ class Provider(generate_utils.ResumeProvider):
         )
 
     def school(self):
-        template = rnd.choices(self._school_templates, weights=[1.0, 0.5, 0.25], k=1)[0]
+        template = self.generator.random_template_weighted(self._school_templates)
         return template.format(
             school_name=self.school_name(),
             field_sep_dt=self.field_sep_dt(),
@@ -183,7 +181,7 @@ class Provider(generate_utils.ResumeProvider):
         )
 
     def university(self):
-        template = rnd.choices(self._university_templates, weights=[1.0, 0.5, 0.25], k=1)[0]
+        template = self.generator.random_template_weighted(self._university_templates)
         return template.format(
             university_name=self.university_name(),
             field_sep_dt=self.field_sep_dt(),
@@ -195,16 +193,8 @@ class Provider(generate_utils.ResumeProvider):
         return rnd.choice(c.UNIVERSITY_DEGREES)
 
     def university_name(self):
-        template = rnd.choices(
-            self._university_name_templates,
-            weights=[1.0, 1.0, 1.0, 0.5, 0.25],
-            k=1,
-        )[0]
-        place_name_template = rnd.choices(
-            self._university_name_place_name_templates,
-            weights=[1.0, 1.0, 0.5],
-            k=1,
-        )[0]
+        template = self.generator.random_template_weighted(self._university_name_templates)
+        place_name_template = self.generator.random_template_weighted(self._university_name_place_name_templates)
         city = self.generator.city()
         state = self.generator.state()
         return template.format(
@@ -222,11 +212,9 @@ class Provider(generate_utils.ResumeProvider):
                 sep=rnd.choice([", ", " – ", "-", "–", " ", " at "]),
             ),
             uni_subunit=rnd.choice(c.UNIVERSITY_SUBUNITS),
-            uni_type=rnd.choices(
-                c.UNIVERSITY_TYPES,
-                weights=[1.0, 1.0, 0.5, 0.5, 0.25, 0.25],
-                k=1,
-            )[0],
+            uni_type=self.generator.random_element_weighted(
+                c.UNIVERSITY_TYPES, [1.0, 1.0, 0.5, 0.5, 0.25, 0.25]
+            ),
         )
 
 
@@ -237,7 +225,7 @@ FAKER.add_provider(Provider)
 FIELDS = {
     "area": (FAKER.area_of_study, "area"),
     "area_detail": (FAKER.area_detail, "area"),
-    "bullet": (lambda: "- ", "bullet"),
+    "bullet": (FAKER.bullet_point, "field_sep"),
     "city_state": (FAKER.city_state, "institution"),
     "course": (FAKER.course_title, "course"),
     "deg_school": (FAKER.school_degree, "study_type"),
@@ -254,7 +242,8 @@ FIELDS = {
     "label_gpa": (FAKER.label_gpa, "field_label"),
     "label_dt": (FAKER.label_grad_date, "field_label"),
     "lb": (FAKER.left_bracket, "field_sep"),
-    "nl": (FAKER.newline, "field_sep"),
+    "nl": (fnc.partial(FAKER.newline, nrange=(1, 3), weights=[1.0, 0.1]), "field_sep"),
+    "nls": (fnc.partial(FAKER.newline, nrange=(1, 3), weights=[0.1, 1.0]), "field_sep"),
     "rb": (FAKER.right_bracket, "field_sep"),
     "school": (FAKER.school, "institution"),
     "uni": (FAKER.university, "institution"),
@@ -265,13 +254,12 @@ Dict[str, Tuple[Callable, str]]: Mapping of field key string to a function that 
 a random field value and the default field label assigned to the value.
 """
 
-#############################
-## random group generators ##
-#############################
+######################################
+## random field template generators ##
 
-def generate_group_courses():
+def course_list_template():
     """
-    Generate a template for a logical group of course fields, consisting of a label and
+    Generate a template for a group of course fields, consisting of a label and
     a variable number of course titles separated by appropriate punctuation,
     possibly split over two lines.
     """
@@ -285,13 +273,13 @@ def generate_group_courses():
     return template.format(
         course_list=" {isep} ".join(
             "{course}" if i != newline_idx else "{nl} {course}"
-            for i in range(rnd.randint(minn, maxn))),
+            for i in range(rnd.randrange(minn, maxn))),
     )
 
 
-def generate_group_courses_block():
+def course_block_template():
     """
-    Generate a template for a logical group of course fields formatted as a sub-headered
+    Generate a template for a group of course fields formatted as a sub-headered
     block, consisting of a header (label) and a variable number of course titles,
     either as an optionally bulletted list split over one or multiple lines.
     """
@@ -308,16 +296,16 @@ def generate_group_courses_block():
     )[0]
     course_list_sep = rnd.choices([" {isep} ", " {ws} "], weights=[1.0, 0.2], k=1)[0]
     return template.format(
-        course_lines=" {bullet} ".join("{course}" for _ in range(rnd.randint(3, 5))),
+        course_lines=" {bullet} ".join("{course}" for _ in range(rnd.randrange(3, 6))),
         course_list=course_list_sep.join(
             "{course}" if i != newline_idx else "{nl} {course}"
-            for i in range(rnd.randint(minn, maxn))),
+            for i in range(rnd.randrange(minn, maxn))),
     )
 
 
-def generate_group_date():
+def date_group_template():
     """
-    Generate a template for a logical group of date fields, consisting of an end date,
+    Generate a template for a group of date fields, consisting of an end date,
     possibly preceded by a label and/or a start date.
     """
     templates = (
@@ -328,9 +316,9 @@ def generate_group_date():
     return rnd.choices(templates, weights=[1.0, 0.5, 0.1], k=1)[0]
 
 
-def generate_group_study():
+def study_group_template():
     """
-    Generate a template for a logical group of study fields, consisting of a degree,
+    Generate a template for a group of study fields, consisting of a degree,
     a primary area of study, and possibly a secondary area of study.
     """
     templates = (
@@ -342,32 +330,32 @@ def generate_group_study():
 
 
 _EXPERIENCES = [
-    lambda: "{uni} {nl}" + generate_group_study() + "{fsep_sm|ws|nl}" + generate_group_date(),
-    lambda: "{uni} {fsep_sm|ws}" + generate_group_date() + "{nl}" + generate_group_study(),
-    lambda: "{uni} {fsep_sm|ws}" + generate_group_date() + "{nl}" + generate_group_study() + "{nl}" + rnd.choice([generate_group_courses(), generate_group_courses_block()]),
-    lambda: generate_group_study() + "{nl} {uni} {fsep_sm|ws|nl}" + generate_group_date(),
+    lambda: "{uni} {nl}" + study_group_template() + "{fsep_sm|ws|nl}" + date_group_template(),
+    lambda: "{uni} {fsep_sm|ws}" + date_group_template() + "{nl}" + study_group_template(),
+    lambda: "{uni} {fsep_sm|ws}" + date_group_template() + "{nl}" + study_group_template() + "{nl}" + rnd.choice([course_list_template(), course_block_template()]),
+    lambda: study_group_template() + "{nl} {uni} {fsep_sm|ws|nl}" + date_group_template(),
     lambda: (
-        "{uni} {fsep_sm|ws}" + generate_group_date() +
-        "{nl}" + generate_group_study() +
+        "{uni} {fsep_sm|ws}" + date_group_template() +
+        "{nl}" + study_group_template() +
         "{nl|fsep_sm} {label_gpa::0.75} {gpa}"
     ),
     lambda: " {nl} {bullet} ".join(
         ["{uni}"] + rnd.sample(
-            ["{label_gpa} {gpa}", "{area_detail}", generate_group_study(), generate_group_date(), generate_group_courses()],
-            rnd.randint(2, 3)
+            ["{label_gpa} {gpa}", "{area_detail}", study_group_template(), date_group_template(), course_list_template()],
+            rnd.randrange(2, 4)
         )
     ),
     lambda: " {nl} ".join(
         ["{uni}"] + rnd.sample(
-            ["{label_gpa} {gpa}", "{area_detail}", generate_group_study(), generate_group_date(), generate_group_courses()],
-            rnd.randint(2, 3)
+            ["{label_gpa} {gpa}", "{area_detail}", study_group_template(), date_group_template(), course_list_template()],
+            rnd.randrange(2, 4)
         )
     ),
     lambda: " {nl} ".join(
         ["{uni}"] + rnd.sample(
-            ["{label_gpa} {gpa}", generate_group_study(), generate_group_date()],
-            rnd.randint(2, 3)
-        ) + [generate_group_courses_block()]
+            ["{label_gpa} {gpa}", study_group_template(), date_group_template()],
+            rnd.randrange(2, 4)
+        ) + [course_block_template()]
     ),
     lambda: "{school} {fsep_sm|ws} {label_dt::0.5} {dt} {fsep_sm|nl|ws} {deg_school}",
 ]
@@ -379,12 +367,12 @@ in a variety of formats and with a variety of constituent fields.
 
 TEMPLATES = [
     # 1 or 2 of the same experience
-    lambda: " {nl} {nl} ".join(
+    lambda: " {nls} {nl::0.25} ".join(
         exp()
         for exp in [rnd.choice(_EXPERIENCES)] * rnd.choices([1, 2], weights=[1.0, 0.33], k=1)[0]
     ),
     # 1 or 2 of different experiences
-    lambda: " {nl} {nl} ".join(
+    lambda: " {nls} {nl::0.25} ".join(
         exp()
         for exp in rnd.sample(_EXPERIENCES, rnd.choices([1, 2], weights=[1.0, 0.33], k=1)[0])
     ),
