452d5b0d2e0949866d573ac2036f129c5077f14a
Alivia Blount <aliviablount@gmail.com>
2020-04-06 07:10:38 -0700

    add index file

--- upload.py
+++ upload.py
@@ -21,7 +21,6 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    print(UPLOAD_FOLDER)
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
